21c6d84cf03672c5b87f00440ec93d03ff88f568
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2023-06-14 11:29:24 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# WooCommerce Total Sales Shortcode
+
+This plugin will provide shortcode for total sales for Week To Date (WTD), Month To Date (MTD) and Year To Date (YTD).
+
+## Installation
+
+Upload this repository to your WordPress Dashboard --> Plugins --> Add New --> Upload
+
+## Configuration
+
+Place shortcode inside your page and it will display the total sales. Available shortcode is:
+
+    - `woocommerce_wtd_sales` for Week To Date sales amount
+    - `woocommerce_mtd_sales` for Month To Date sales amount
+    - `woocommerce_ytd_sales` for Year To Date sales amount
+
+## Limits
+
+To avoid significant performance issues, the plugin has configured to count only up to 200 orders. However, it can be changed depending on your needs.
---
+++ woocommerce_total_sales.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Plugin Name: WooCommerce Total Sales (WTD, MTD, YTD)
+ * Description: Add total sales counter shortcode for WooCommerce.
+ * Version: 1.0.0
+ * Author: Wan Zulkarnain
+ */
+
+add_shortcode('woocommerce_wtd_sales', 'woocommerce_wan_wtd_sales');
+add_shortcode('woocommerce_mtd_sales', 'woocommerce_wan_mtd_sales');
+add_shortcode('woocommerce_ytd_sales', 'woocommerce_wan_ytd_sales');
+
+function woocommerce_wan_wtd_sales() {
+
+  if ( !is_user_logged_in() ) {
+    return wc_price( 0 );
+  }
+
+  $day = date('w');
+  $week_start = date('Y-m-d', strtotime('-'.$day.' days'));
+  $week_end = date('Y-m-d', strtotime('+'.(6-$day).' days'));
+
+  $user_id = get_current_user_id();
+
+  $wtd_orders = wc_get_orders( 
+    [
+      'date_completed' => $week_start . '...' . $week_end,
+      'customer_id' => $user_id,
+      'limit' => 200,
+    ]
+  );
+
+  $wtd_total = 0;
+
+  foreach( $wtd_orders as $order ) {
+    $wtd_total+= round( $order->get_total() * 100);
+  }
+
+  return wc_price( $wtd_total / 100 );
+}
+
+function woocommerce_wan_mtd_sales() {
+
+  if ( !is_user_logged_in() ) {
+    return wc_price( 0 );
+  }
+
+  $user_id = get_current_user_id();
+
+  $mtd_orders = wc_get_orders(
+    [
+      'date_completed' => date('Y-m-01') . '...' . date('Y-m-t'),
+      'customer_id' => $user_id,
+      'limit' => 200,
+    ] 
+  );
+
+  $mtd_total = 0;
+
+  foreach( $mtd_orders as $order ) {
+    $mtd_total+= round( $order->get_total() * 100);
+  }
+
+  return wc_price( $mtd_total / 100);
+}
+
+function woocommerce_wan_ytd_sales() {
+
+  if ( !is_user_logged_in() ) {
+    return wc_price( 0 );
+  }
+
+  $user_id = get_current_user_id();
+
+  $ytd_orders = wc_get_orders(
+    [
+      'date_completed' => date('Y-01-01') . '...' . date('Y-m-t'),
+      'customer_id' => $user_id,
+      'limit' => 200,
+    ] 
+  );
+
+  $ytd_total = 0;
+
+  foreach( $ytd_orders as $order ) {
+    $ytd_total+= round( $order->get_total() * 100);
+  }
+
+  return wc_price( $ytd_total / 100);
+}
